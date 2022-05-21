6d87f4287a1b95eb92fcc4ed29bb6ab8a33aec75
Rodrigo Gomes <cklrod@hotmail.com>
2022-05-21 19:41:17 -0300

    erros e stacktrace

---
+++ erros.js
@@ -0,0 +1,6 @@
+//const numero; //Missing initializer in const declaration
+
+// const numero = 1;
+// console.log(numero; //missing ) after argument list
+
+// console.log(variavel); //missing ) after argument list
