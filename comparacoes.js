066b265b6b267d7f1e8f7b3336440fe3b41ac4bf
Rodrigo Gomes <cklrod@hotmail.com>
2022-05-21 20:23:01 -0300

    operadores de comparação

---
+++ comparacoes.js
@@ -0,0 +1,14 @@
+// == comparação implícita
+
+console.log ('1' == 1);
+
+//=== comparacao explícita
+
+console.log ('1'=== 1);
+
+// == compara só o valor
+// === compara valor e tipo
+// || retorana true se uma das condições for verdadeira
+// && retorna true se todas condições for verdadeira
+// != compara se o valor é diferente
+// !== compara se o valor e o tipo for diferente
