76040a0354b867e89d1fbf070afb939540f00c51
Rodrigo Gomes <cklrod@hotmail.com>
2022-05-21 21:19:23 -0300

    entendo expressões de função

---
+++ expressoes-funcao.js
@@ -0,0 +1,14 @@
+//declaração de função
+function minhaFuncao(param){
+    //bloco de código
+}
+
+minhaFuncao("param");
+
+//expressão  de função
+
+const soma = function(num1, num2){return num1 + num2;}
+
+console.log(soma(2,3));
+
+// javascript primeiro executa "lista" todas as VAR e funções antes de executar o código
